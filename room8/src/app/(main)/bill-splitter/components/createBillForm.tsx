8fb33f21552c70e83f994e81b90f77223d956ff5
Maged Armanios <maged.armanios@yahoo.com>
2024-11-11 00:12:31 -0500

    WIP: Create bill page

---
+++ room8/src/app/(main)/bill-splitter/components/createBillForm.tsx
@@ -0,0 +1,132 @@
+'use client';
+
+import React, { useState } from 'react';
+import useRoommates from '@/hooks/useRoommates';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
+import { z } from 'zod';
+
+import { Button } from '@/components/ui/button';
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage
+} from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
+
+const formSchema = z.object({
+  name: z.string(),
+  amount: z.number(),
+  equally: z.boolean(),
+  debts: z.array(z.map(z.string(), z.number()))
+});
+
+export default function CreateBillForm() {
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [error, setError] = useState<string | null>(null);
+  const [debts, setDebts] = useState<Array<{ key: string; value: number }>>([]);
+
+  const { data: roommates, status } = useRoommates();
+
+  async function onSubmit(values: z.infer<typeof formSchema>) {
+    console.log(values);
+  }
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: '',
+      amount: 0,
+      equally: false,
+      debts: []
+    }
+  });
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        <FormField
+          control={form.control}
+          name="name"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Bill Name</FormLabel>
+              <FormControl>
+                <Input placeholder="Ex: December Internet Bill" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="amount"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Bill Amount</FormLabel>
+              <FormControl>
+                <Input type="number" step={0.01} placeholder="Ex: 10.24" {...field} />
+              </FormControl>
+              <FormDescription>Enter the amount you paid</FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="amount"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Split the bill equally</FormLabel>
+              <FormControl>
+                <Input type="checkbox" {...field} />
+              </FormControl>
+              <FormMessage />
+              <FormDescription>
+                Check this off if you are splitting the bill equally
+              </FormDescription>
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="debts"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Debtors</FormLabel>
+              <FormControl>
+                <div>
+                  {roommates &&
+                    roommates.map((roommate, index) => (
+                      <div key={index}>
+                        <label>
+                          {roommate.name}:
+                          <input
+                            type="number"
+                            onChange={(e) =>
+                              setDebts([
+                                ...debts,
+                                { key: roommate.id, value: e.target.value as unknown as number }
+                              ])
+                            }
+                            placeholder="Enter how much they owe"
+                          />
+                        </label>
+                      </div>
+                    ))}
+                </div>
+              </FormControl>
+              <FormMessage />
+              <FormDescription>Select who owes you money for this bill</FormDescription>
+            </FormItem>
+          )}
+        />
+        <Button type="submit">Submit</Button>
+      </form>
+    </Form>
+  );
+}
--- room8/src/app/(main)/bill-splitter/page.tsx
+++ room8/src/app/(main)/bill-splitter/page.tsx
@@ -1,3 +1,61 @@
+'use client';
+
+import { useState } from 'react';
+import { FilePen, House, HandCoins, Receipt } from 'lucide-react';
+import { cn } from '@/lib/utils';
+import CreateBillForm from './components/createBillForm';
+
+type Tab = {
+  icon: JSX.Element;
+  title: string;
+  component: JSX.Element;
+};
+
+type Tabs = {
+  [key: string]: Tab;
+};
+
 export default function BillSplitterPage() {
-  return <div>BillSplitterPage</div>;
+  const TABS: Tabs = {
+    summary: { icon: <House />, title: 'Summary', component: <div>Summary</div> },
+    create: {
+      icon: <FilePen />,
+      title: 'Create Bill',
+      component: <CreateBillForm />
+    },
+    outstanding: {
+      icon: <HandCoins />,
+      title: 'Outstanding Bills',
+      component: <div>Outstanding</div>
+    },
+    pending: {
+      icon: <Receipt />,
+      title: 'Pending Bills',
+      component: <div>Pending</div>
+    }
+  };
+
+  const [activeTab, setActiveTab] = useState(TABS.summary);
+  return (
+    <div>
+      <nav className={cn('flex justify-center gap-6 ')}>
+        {Object.values(TABS).map((tab) => (
+          <div
+            key={tab.title}
+            onClick={() => setActiveTab(tab)}
+            className={cn(
+              'flex gap-3 cursor-pointer hover:bg-[#955363] p-2 rounded-md transition-all',
+              {
+                'bg-[#490024]': activeTab.title === tab.title
+              }
+            )}>
+            {tab.icon}
+            {tab.title}
+          </div>
+        ))}
+      </nav>
+      <hr />
+      {activeTab.component}
+    </div>
+  );
 }
