fbc234039c1d49f69c237f072a46f929e7eac7f6
jane-klavir <97529643+jane-klavir@users.noreply.github.com>
2025-02-06 22:15:36 -0500

    fixed COCO list, ensured proper masked images being passed to algorithm

--- cleanliness_detection/temp.py
+++ cleanliness_detection/temp.py
@@ -19,19 +19,16 @@ DIST_THRESH = 15                                                                
 IOU_THRESH = 0.8                                                                            # min IOU threshold for object to be considered in same spot
 # Faster R-CNN is trained on COCO dataset
 COCO_LABELS = {
-    0: "background", 1: "person", 2: "bicycle", 3: "car", 4: "motorcycle", 5: "airplane", 6: "bus", 7: "train",
-    8: "truck", 9: "boat", 10: "traffic light", 11: "fire hydrant", 12: "street sign", 13: "stop sign",
-    14: "parking meter", 15: "bench", 16: "bird", 17: "cat", 18: "dog", 19: "horse", 20: "sheep", 21: "cow",
-    22: "elephant", 23: "bear", 24: "zebra", 25: "giraffe", 26: "hat", 27: "backpack", 28: "umbrella",
-    29: "shoe", 30: "eye glasses", 31: "handbag", 32: "tie", 33: "suitcase", 34: "frisbee", 35: "skis",
-    36: "snowboard", 37: "sports ball", 38: "kite", 39: "baseball bat", 40: "baseball glove", 41: "skateboard",
-    42: "surfboard", 43: "tennis racket", 44: "bottle", 45: "plate", 46: "wine glass", 47: "cup", 48: "fork",
-    49: "knife", 50: "spoon", 51: "bowl", 52: "banana", 53: "apple", 54: "sandwich", 55: "orange", 56: "broccoli",
-    57: "carrot", 58: "hot dog", 59: "pizza", 60: "donut", 61: "cake", 62: "chair", 63: "couch", 64: "potted plant",
-    65: "bed", 66: "mirror", 67: "dining table", 68: "window", 69: "desk", 70: "toilet", 71: "door", 72: "TV",
-    73: "laptop", 74: "mouse", 75: "remote", 76: "keyboard", 77: "cell phone", 78: "microwave", 79: "oven",
-    80: "toaster", 81: "sink", 82: "refrigerator", 83: "blender", 84: "book", 85: "clock", 86: "vase", 87: "scissors",
-    88: "teddy bear", 89: "hair drier", 90: "toothbrush"
+    0: "person", 1: "bicycle", 2: "car", 3: "motorcycle", 4: "airplane", 5: "bus", 6: "train", 7: "truck", 8: "boat",
+    9: "traffic light", 10: "fire hydrant", 11: "stop sign", 12: "parking meter", 13: "bench", 14: "bird", 15: "cat", 
+    16: "dog", 17: "horse", 18: "sheep", 19: "cow", 20: "elephant", 21: "bear", 22: "zebra", 23: "giraffe", 24: "backpack", 
+    25: "umbrella", 26: "handbag", 27: "tie", 28: "suitcase", 29: "frisbee", 30: "skis", 31: "snowboard", 32: "sports ball", 
+    33: "kite", 34: "baseball bat", 35: "baseball glove", 36: "skateboard", 37: "surfboard", 38: "tennis racket", 39: "bottle", 
+    40: "wine glass", 41: "cup", 42: "fork", 43: "knife", 44: "spoon", 45: "bowl", 46: "banana", 47: "apple", 48: "sandwich", 
+    49: "orange", 50: "broccoli", 51: "carrot", 52: "hot dog", 53: "pizza", 54: "donut", 55: "cake", 56: "chair", 57: "couch", 
+    58: "potted plant", 59: "bed", 60: "dining table", 61: "toilet", 62: "TV", 63: "laptop", 64: "mouse", 65: "remote", 
+    66: "keyboard", 67: "cell phone", 68: "microwave", 69: "oven", 70: "toaster", 71: "sink", 72: "refrigerator", 73: "book", 
+    74: "clock", 75: "vase", 76: "scissors", 77: "teddy bear", 78: "hair drier", 79: "toothbrush"
 }
 
 """Define a class to represent a detection"""
@@ -222,18 +219,19 @@ class CleanlinessDetector:
         centroids_before = np.array([obj.centroid for obj in objects_before])
         centroids_after = np.array([obj.centroid for obj in objects_after])
 
-        nbrs = NearestNeighbors(n_neighbors=1).fit(centroids_before)
-        dists, inds = nbrs.kneighbors(centroids_after)
+        if len(centroids_before) > 0 and len(centroids_after) > 0:
+            nbrs = NearestNeighbors(n_neighbors=1).fit(centroids_before)
+            dists, inds = nbrs.kneighbors(centroids_after)
 
-        objects_before_cp = objects_before.copy()
-        objects_after_cp = objects_after.copy()
+            objects_before_cp = objects_before.copy()
+            objects_after_cp = objects_after.copy()
 
-        # case 1: object didn't move (disregard these objects)
-        for obj2, dist, idx in zip(objects_after_cp, dists, inds):
-            obj1 = objects_before_cp[idx[0]]
-            if dist <= DIST_THRESH and obj1.iou(obj2) > IOU_THRESH and obj1.class_num == obj2.class_num:
-                objects_before.remove(obj1)
-                objects_after.remove(obj2)
+            # case 1: object didn't move (disregard these objects)
+            for obj2, dist, idx in zip(objects_after_cp, dists, inds):
+                obj1 = objects_before_cp[idx[0]]
+                if dist <= DIST_THRESH and obj1.iou(obj2) > IOU_THRESH and obj1.class_num == obj2.class_num:
+                    objects_before.remove(obj1)
+                    objects_after.remove(obj2)
 
         # case 2: object was added to scene
         objects_after_cp = objects_after.copy()
@@ -378,8 +376,8 @@ class CleanlinessDetector:
 if __name__ == "__main__":
     cd = CleanlinessDetector()
     # Process images
-    before_img = Image.open("cleanliness_detection/samples/5/before.jpeg")
-    after_img = Image.open("cleanliness_detection/samples/5/after.jpeg")
+    before_img = Image.open("cleanliness_detection/samples/1/before.png")
+    after_img = Image.open("cleanliness_detection/samples/1/after.png")
 
     [before_mask, after_mask] = cd.combine_image_mask(before_img, after_img, display=True)
 
@@ -395,7 +393,7 @@ if __name__ == "__main__":
     after_name_to_id = {obj.class_name: obj for obj in objects_after}
 
     # Calculate the differences
-    added, removed, moved = cd.calculate_difference(before_img, after_img)
+    added, removed, moved = cd.calculate_difference(before_mask, after_mask)
 
     # Extract class names for printing
     added_names = [obj.class_name for obj in added]
