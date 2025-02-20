c19a547f41a4866311de1bb9691e5a6ec7e987b2
jinalkast <jinalkast@gmail.com>
2025-02-20 12:54:22 -0500

    updated chatbot functionality

--- room8/src/app/(main)/chatbot/hooks/useActivateChatbot.ts
+++ room8/src/app/(main)/chatbot/hooks/useActivateChatbot.ts
@@ -1,16 +1,19 @@
 import { Json } from '@/lib/types/supabase';
 import { useMutation } from '@tanstack/react-query';
 import useRoommates from '../../../../hooks/useRoommates';
+import useGetHouse from '@/hooks/useGetHouse';
 import { TRoommate } from '@/lib/types';
+import { THouse } from '../../house-settings/types';
 
-export const activateChatbot = async (roommates: TRoommate[] | null): Promise<Json | null> => {
+export const activateChatbot = async (roommates: TRoommate[] | null, house: THouse | null): Promise<Json | null> => {
   const phoneList = roommates
     ? roommates.map((roommate) => roommate.phone).filter((phone) => phone != 'NULL')
     : [];
+  const houseId = house ? house.id : '';
   const res = await fetch(`/api/chatbot`, {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({ participants: phoneList })
+    body: JSON.stringify({ participants: phoneList, house: houseId })
   });
 
   const json = await res.json();
@@ -19,13 +22,17 @@ export const activateChatbot = async (roommates: TRoommate[] | null): Promise<Js
 
 export default function useActivateChatbot() {
   const { data: roommates } = useRoommates();
+  const { data: house } = useGetHouse();
 
   return useMutation({
     mutationFn: () => {
       if (!roommates) {
         throw new Error('Error getting roommates');
       }
-      return activateChatbot(roommates);
+      if (!house) {
+        throw new Error('Error getting house');
+      }
+      return activateChatbot(roommates, house);
     }
   });
 }
