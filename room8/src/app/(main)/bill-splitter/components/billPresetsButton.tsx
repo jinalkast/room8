dfd074d784953af293836b8db0a68a180bf3f592
Maged Armanios <maged.armanios@yahoo.com>
2025-03-18 16:23:00 -0400

    Bill presets added

---
+++ room8/src/app/(main)/bill-splitter/components/billPresetsButton.tsx
@@ -0,0 +1,58 @@
+import { Button } from '@/components/ui/button';
+import { Trash } from 'lucide-react';
+import React from 'react';
+import { TBillPreset } from '../types';
+import useDeleteBillPreset from '../hooks/deleteBillPreset';
+import { useToast } from '@/hooks/useToast';
+import { useQueryClient } from '@tanstack/react-query';
+
+const BillPresetsButton = ({
+  billPreset,
+  applyPreset
+}: {
+  billPreset: TBillPreset;
+  applyPreset: (preset: TBillPreset) => void;
+}) => {
+  const { toast } = useToast();
+  const { mutate: deletePreset, isPending: isPresetDeleting } = useDeleteBillPreset({
+    queryClient: useQueryClient(),
+    onSuccessCallback() {
+      toast({
+        title: 'Success',
+        description: 'Preset successfully deleted'
+      });
+    },
+    onErrorCallback() {
+      toast({
+        title: 'Error!',
+        description: 'Failed to delete the preset'
+      });
+    }
+  });
+
+  const handleApplyPreset = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    e.preventDefault();
+    applyPreset(billPreset);
+  };
+
+  const handleDeletePreset = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    e.preventDefault();
+    deletePreset(billPreset.id);
+  };
+
+  return (
+    <Button onClick={handleApplyPreset} className="max-w-[120px]">
+      {billPreset.name}
+      <Button
+        size="icon"
+        variant="ghost"
+        className="p-1 !h-6 !w-6"
+        disabled={isPresetDeleting}
+        onClick={handleDeletePreset}>
+        <Trash className="!h-4 !w-4" fill={'none'} stroke={'currentColor'} />
+      </Button>
+    </Button>
+  );
+};
+
+export default BillPresetsButton;
---
+++ room8/src/app/(main)/bill-splitter/hooks/postBillPresets.ts
@@ -0,0 +1,48 @@
+import { TApiResponse, TBillDB } from '@/lib/types';
+import { QueryClient, useMutation } from '@tanstack/react-query';
+import { postBillSchema } from '@/app/(main)/bill-splitter/types';
+import z from 'zod';
+import { Tables } from '@/lib/types/supabase';
+
+const postBillPreset = async (preset: z.infer<typeof postBillSchema>): Promise<Tables<'bill_presets'>> => {
+  const validatedData = postBillSchema.parse(preset);
+  const res = await fetch(`/api/bills/presets`, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({
+      ...validatedData,
+      owes: Object.fromEntries(validatedData.owes)
+    })
+  });
+
+  if (!res.ok) {
+    throw new Error('Failed to post bill');
+  }
+
+  const json: TApiResponse<Tables<'bill_presets'>> = await res.json();
+  return json.data!;
+};
+
+const usePostBillPreset = ({
+  queryClient,
+  onSuccessCallback,
+  onErrorCallback
+}: {
+  queryClient: QueryClient;
+  onSuccessCallback?: () => void;
+  onErrorCallback?: () => void;
+}) => {
+  return useMutation({
+    mutationKey: ['postBillPreset'],
+    mutationFn: postBillPreset,
+    onSuccess: (result) => {
+      queryClient.invalidateQueries({ queryKey: ['billPresets'] });
+      if (onSuccessCallback) {
+        onSuccessCallback();
+      }
+    },
+    onError: onErrorCallback
+  });
+};
+
+export default usePostBillPreset;
